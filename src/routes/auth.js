ed069d48901e1051cf5e6e958916d2d705ab17b1
atorov <veselin.lkp@gmail.com>
2021-03-25 18:44:55 +0200

    updating project dependencies and adding new route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,14 @@
 import express from 'express'
 
+import checkAuth from '../middleware/check-auth.js'
 import login from '../controllers/auth/login.js'
 
 const router = express.Router()
 
 // public
 router.post('/', login)
+
+// authenticated
+router.get('/', [checkAuth, (_, res) => res.end()])
 
 export default router
