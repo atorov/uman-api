9f0c7b84bc0b2fff45bcffb5bfd712e5c0977fc6
atorov <veselin.lkp@gmail.com>
2022-11-05 18:00:50 +0200

    v2.0.0 updated project deps

--- src/db-connect.js
+++ src/db-connect.js
@@ -4,10 +4,10 @@ import mongoose from 'mongoose'
 const print = debug('db')
 
 async function dbConnect(dbName, dbUser, dbPassword) {
-    mongoose.set('useNewUrlParser', true);
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useUnifiedTopology', true);
+    // mongoose.set('useNewUrlParser', true);
+    // mongoose.set('useFindAndModify', false);
+    // mongoose.set('useCreateIndex', true);
+    // mongoose.set('useUnifiedTopology', true);
 
     try {
         await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.uj68u.mongodb.net/${dbName}?retryWrites=true&w=majority`)
